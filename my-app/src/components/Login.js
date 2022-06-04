fbbc2443d6c40870499ba4aae27003e775a47fb3
MateuszChmiest <mateusz_269@icloud.coml>
2022-06-04 20:18:06 +0200

    work on the firebase and the login form

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,46 +1,115 @@
-import { Button,Form } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "../images/logo.png"
-import user_img from "../images/user.png"
-import { useState } from "react";
+// import { Button, Form } from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../images/logo.png";
+import user_img from "../images/user.png";
+import { useRef, useState } from "react";
+import { signup, singin } from "../firebase";
 
 const Login = () => {
-    const [user, setUser] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordEddor] = useState('');
-    const [hasAccount, setHasAccount] = useState('false')
+	const [hasAccount, setHasAccount] = useState("false");
+	const [emailError, setEmailError] = useState("");
+	const emailRef = useRef();
+	const passwordRef = useRef();
+
+	async function handleSignUp() {
+		try {
+			await signup(emailRef.current.value, passwordRef.current.value);
+		} catch {
+			alert("Error");
+		}
+	}
+
+	async function handleSignIn() {
+		try {
+			await singin(emailRef.current.value, passwordRef.current.value);
+		} catch {
+			alert("Error");
+		}
+	}
 
 	return (
 		<section className='Login'>
-			<h1>Join <span>Us!</span></h1>
+			{hasAccount ? (
+				<h1>
+					Sign <span>In!</span>
+				</h1>
+			) : (
+				<h1>
+					Join <span>Us!</span>
+				</h1>
+			)}
 			<div className='Login__box'>
-                <div className="Login__user">
-                     <img src={user_img} alt="user" width={50}/>
-                </div>
-				<Form>
+				<div className='Login__user'>
+					<img src={user_img} alt='user' width={50} />
+				</div>
+				<div className='Login__form'>
+					<input ref={emailRef} type='email' placeholder='Enter email' />
+					<input type='password' ref={passwordRef} placeholder='Password' />
+					{hasAccount ? (
+						<>
+							<button onClick={handleSignUp}>Sign Up</button>
+                            <p>
+								Don't have an account?
+								<span onClick={() => setHasAccount(!hasAccount)}> Sign up</span>
+							</p>
+						</>
+					) : (
+						<>
+							<button onClick={handleSignIn}>Sign Up</button>
+                            <p>
+								Have an account?
+								<span onClick={() => setHasAccount(!hasAccount)}> Sign in</span>
+							</p>
+						</>
+					)}
+				</div>
+				{/* <Form>
 					<Form.Group className='mb-3' controlId='formBasicEmail'>
 						<Form.Label>Email address</Form.Label>
-						<Form.Control type='email' placeholder='Enter email' />
+						<Form.Control
+							type='email'
+							placeholder='Enter email'
+							ref={emailRef}
+						/>
 						<Form.Text className='text-muted'>
-							We'll never share your email with anyone else.
-						</Form.Text>
-					</Form.Group>
+							We'll never share your email with anyone else. */}
+				{/* </Form.Text> */}
+				{/* <p className='errorMsg'>{emailError}</p> */}
+				{/* </Form.Group> */}
 
-					<Form.Group className='mb-3' controlId='formBasicPassword'>
+				{/* <Form.Group className='mb-3' controlId='formBasicPassword'>
 						<Form.Label>Password</Form.Label>
-						<Form.Control type='password' placeholder='Password' />
-					</Form.Group>
-					<Form.Group className='mb-3' controlId='formBasicCheckbox'>
-						<Form.Check type='checkbox' label='Check me out' />
-					</Form.Group>
-					<Button variant='primary' type='submit'>
-						Login
-					</Button>
-				</Form>
+						<Form.Control
+							type='password'
+							placeholder='Password'
+                            ref={passwordRef}
+						/> */}
+				{/* <p className='errorMsg'>{passwordError}</p> */}
+				{/* </Form.Group>
+					{hasAccount ? (
+						<>
+							<Button variant='primary' type='submit' >
+								Login
+							</Button>
+							<p>
+								Don't have an account?
+								<span onClick={() => setHasAccount(!hasAccount)}> Sign up</span>
+							</p>
+						</>
+					) : (
+						<>
+							<Button variant='primary' type='submit' onClick={handleSignup}>
+								Register
+							</Button>
+							<p>
+								Have an account?
+								<span onClick={() => setHasAccount(!hasAccount)}> Sign in</span>
+							</p>
+						</>
+					)}
+				</Form> */}
 			</div>
-            <img src={logo} alt="logo" width={100}/>
+			<img src={logo} alt='logo' width={100} />
 		</section>
 	);
 };
--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,15 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+import { initializeApp } from "firebase/app";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAb_hNrCsGL4XjfbA__f3d7frHhAcPlVns",
-    authDomain: "yourservicebook.firebaseapp.com",
-    projectId: "yourservicebook",
-    storageBucket: "yourservicebook.appspot.com",
-    messagingSenderId: "104302324327",
-    appId: "1:104302324327:web:4ee3d36061a53bfe3202c2"
-  };
+  apiKey: "AIzaSyAb_hNrCsGL4XjfbA__f3d7frHhAcPlVns",
+  authDomain: "yourservicebook.firebaseapp.com",
+  projectId: "yourservicebook",
+  storageBucket: "yourservicebook.appspot.com",
+  messagingSenderId: "104302324327",
+  appId: "1:104302324327:web:4ee3d36061a53bfe3202c2"
+};
 
-  // Initialize Firebase
-  const firebase = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
 
-  export default firebase;
+//Export Firebase
+export function signup(email,password) {
+    return createUserWithEmailAndPassword(auth,email,password);
+}
+
+export function singin(email,password) {
+    return signInWithEmailAndPassword(auth,email,password)
+}
