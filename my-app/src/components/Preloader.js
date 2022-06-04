55456919c96b12c3e26f774aa23109b772f5497d
MateuszChmiest <mateusz_269@icloud.coml>
2022-06-04 13:55:59 +0200

    preloader component done

---
+++ my-app/src/components/Preloader.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import "../sass/elements/_preloader.scss";
+import logo from "../images/logo.png"
+import BarLoader from "react-spinners/BarLoader";
+
+const Preloader = () => {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 3000);
+	}, []);
+
+	return (
+		<>
+			{loading ? (
+				<div className='Preloader'>
+					<header className='Preloader__header'>
+						<img src={logo} alt='logo' />
+					</header>
+                    <BarLoader color={"#38B6FF"} loading={loading} size={15}/>
+				</div>
+			) : null}
+		</>
+	);
+};
+
+export default Preloader;
