15aa69fc3fedd4b00974f89ca912a84b91234e7a
MateuszChmiest <mateusz_269@icloud.com>
2022-06-06 18:08:26 +0200

    styling hero section

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Hero = () => {
-    return <h1>Hello</h1>
+    return (
+        <section className="Hero">
+            <div className="Hero__menu">
+                <ul className="Hero__list">
+                    <li><a>Your Car</a></li>
+                    <li><a>Exploitation</a></li>
+                    <li><a>Repairs</a></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <button className="Hero__btn"></button>
+            </div>
+            <div className="Hero__dashboard">
+                
+            </div>
+        </section>
+    )
 }
 
 export default Hero;
